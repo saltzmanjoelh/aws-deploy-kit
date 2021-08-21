4aa7c87da2cad3caf87bf966437c55366b2edf90
Joel Saltzman <talk@joelsaltzman.com>
2021-08-20 17:11:29 -0700

    Reverted to Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -72,7 +72,7 @@ let package = Package(
                 .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
             ]
         ),
-        .executableTarget(
+        .target(
             name: "aws-deploy",
             dependencies: [
                 "AWSDeployCore",
